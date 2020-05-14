0f252ba77079d941aa4fa0bf8da57e7ef59a38ee
swapnilkusumwal <swapnilkusumwal@gmail.com>
2020-05-15 02:19:13 +0530

    Callback Hell and Promises

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -4,31 +4,37 @@ const dboper=require('./operations');
 const url="mongodb://localhost:27017/";
 const dbname='confusion';
 
-MongoClient.connect(url,(err,client)=>{
-    assert.equal(err,null);
-
+MongoClient.connect(url).then((client)=>{
     console.log("Connected to server...\n");
 
     const db=client.db(dbname);
     
-    dboper.insertDocument(db,{"name":"Vadonut","description":"test"},'dishes',(result)=>{
+    dboper.insertDocument(db,{"name":"Vadonut","description":"test"},'dishes')
+    .then((result)=>{
         console.log("Insert Document:\n",result.ops);
 
-        dboper.findDocuments(db,'dishes',(docs)=>{
-            console.log("Found documents:\n",docs);
+        return dboper.findDocuments(db,'dishes')
+    })
+    .then((docs)=>{
+        console.log("Found documents:\n",docs);
 
-            dboper.updateDocument(db,{"name":"vadonut"},{"description":"Updated test"},'dishes',(result)=>{
-                console.log("Update document:\n",result.result);
-                
-                dboper.findDocuments(db,'dishes',(docs)=>{
-                    console.log("Found documents:\n",docs);
+        return dboper.updateDocument(db,{"name":"vadonut"},{"description":"Updated test"},'dishes')
+    })
+    .then((result)=>{
+        console.log("Update document:\n",result.result);
+        
+        dboper.findDocuments(db,'dishes')
+    })
+    .then((docs)=>{
+        console.log("Found documents:\n",docs);
 
-                    db.dropCollection('dishes',(result)=>{
-                        console.log("Dropper collection: "+result);
-                        client.close();
-                    })
-                });
-            });
-        });
-    });
-});
+        return db.dropCollection('dishes')
+    })
+    
+    .then((result)=>{
+            console.log("Dropper collection: "+result);
+            client.close();
+    })
+    .catch((err)=>console.log(err));         
+})
+.catch((err)=>console.log(err));
