59e460c2466ad1eff57183773ca8f96bb3b1ee15
swapnilkusumwal <43251574+swapnilkusumwal@users.noreply.github.com>
2020-05-26 05:44:17 +0530

    Delete config.js

--- confusion/config.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/confusion',
-    'facebook':{
-        clientId:'688722215298591',
-        clientSecret:'3bc86c9b7129dfc379febbd8dc182e89'
-    }
-}
