807732542bcbbc362ac04a552fbde2113e3e1dbf
swapnilkusumwal <swapnilkusumwal@gmail.com>
2020-05-26 05:50:36 +0530

    .

---
+++ confusion/config.js
@@ -0,0 +1,8 @@
+module.exports={
+    'secretKey': 'whatever your key is',
+    'mongoUrl': 'mongodb://localhost:27017/confusion',
+    'facebook':{
+        clientId:'<Clientid>',
+        clientSecret:'<clientSecret>'
+    }
+}
